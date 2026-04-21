bba063be84295b72276cddacd48e9bafa4230a95
SambhavRaghuvanshi <sambhavraghuvanshi17@gmail.com>
2026-04-21 19:09:14 +0530

    Update db.php

--- include/db.php
+++ include/db.php
@@ -1,1 +1,13 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "manager";
+$database = "placement_cell";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
